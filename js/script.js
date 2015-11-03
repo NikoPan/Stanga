ff67b30b1e402e52d3ada71481df78d4fbd7495c
Nikolay Panayotov <panayotov.nikolay@gmail.com>
2015-11-03 14:01:22 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-/**
- * Created by Nikolay on 28.10.2015 ã..
- */
+$(document).ready(function () {
+
+    $(".menu").click(function () {
+        $(".navigation").toggle();
+    });
+    $("#profile").click(function () {
+        $(".submenu").toggle();
+    });
+
+    $("#industrial-btn").click(function () {
+        $("#industrial-nfo").show();
+        $("#challenges-nfo").hide();
+
+    });
+    $("#industrial-x").click(function () {
+        $("#industrial-nfo").hide();
+    });
+
+    $("#challenges-btn").click(function () {
+        $("#challenges-nfo").show();
+    });
+    $("#challenges-x").click(function () {
+        $("#challenges-nfo").hide();
+    });
+
+});
